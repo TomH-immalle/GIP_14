df96bd4564c7dc43aab0d799133e9d527cdf11c0
Tom Hollestelle <tom.hollestelle@immalle.eu>
2018-05-02 13:42:43 +0200

    Add project files.

---
+++ JobsISeaYouAt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobsISeaYouAt.Startup))]
+namespace JobsISeaYouAt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
